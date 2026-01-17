dc0a9b16598dff1c01060ce161853070edbbcad5
hantianzhun <hl14332232020@outlook.com>
2026-01-17 16:18:13 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -44,10 +44,7 @@ export default {
       url.pathname === '/' ||
       url.pathname === '/index.html' ||
       url.pathname === '/github.html'
-    ) {
-      return env.ASSETS.fetch(
-        new Request('https://assets.local/github.html')
-      )
+    ) {return Response.redirect(new URL('/github.html', request.url).toString(), 302)
     }
 
 
@@ -199,6 +196,7 @@ async function proxy(urlObj, reqInit) {
     headers
   })
 }
+
 
 
 
